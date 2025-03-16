1815246ce4ae03739b96cc38fffa40c8500ba98c
nguyendangtu <nguyendangtu1987@gmail.com>
2025-03-16 15:03:06 -0500

    update image

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t nguyendangtu/spring-argocd-app:v1 .
-docker push nguyendangtu/spring-argocd-app:v1
+docker build -t johnguyen/spring-argocd-app:v1 .
+docker push johnguyen/spring-argocd-app:v1
